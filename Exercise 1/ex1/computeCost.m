5b702fcd353c0fa1f427221ba1f2cf92be594ad9
amangang3 <amangang3@gmail.com>
2020-08-01 21:38:53 +0400

    Updated the cost function

--- Exercise 1/ex1/computeCost.m
+++ Exercise 1/ex1/computeCost.m
@@ -12,10 +12,10 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
+for i = 1:m
+  individual_cost(i) = ((theta(1) + theta(2)*X(i)) - y(i))^2;
+end
+J = (1/(2*m)) * (sum(individual_cost))
 
 % =========================================================================
 
