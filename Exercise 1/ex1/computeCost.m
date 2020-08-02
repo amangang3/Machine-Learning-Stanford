74b4650a668f642721535dfd5885bd82d17af589
amangang3 <amangang3@gmail.com>
2020-08-02 12:55:20 +0400

    finished Ex1 manditory

--- Exercise 1/ex1/computeCost.m
+++ Exercise 1/ex1/computeCost.m
@@ -12,10 +12,7 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-for i = 1:m
-  individual_cost(i) = ((theta(1) + theta(2)*X(i)) - y(i))^2;
-end
-J = (1/(2*m)) * (sum(individual_cost))
+  J = (1/(2*m))*sum(((X*theta)-y).^2);
 
 % =========================================================================
 
--- Exercise 1/ex1/gradientDescent.m
+++ Exercise 1/ex1/gradientDescent.m
@@ -16,18 +16,18 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
+    
+    error = (X * theta) - y;
+    theta_1 = theta(1) - ((alpha/m) * sum(error.*X(:,1)));
+    theta_2 = theta(2) - ((alpha/m) * sum(error.*X(:,2)));
+    theta = [theta_1; theta_2];
+        
+    end
     % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
-end
+
 
 end
