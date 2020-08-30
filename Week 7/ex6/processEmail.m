f53d9e4f04680032f6fd018aaee749f216a8bfa1
amangang3 <amangang3@gmail.com>
2020-08-30 12:39:40 +0400

    implimented SVM for spam email classifier

--- Week 7/ex6/processEmail.m
+++ Week 7/ex6/processEmail.m
@@ -95,8 +95,12 @@ while ~isempty(email_contents)
     % 
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
-    %
-
+    
+    matching_words = strcmp(str, vocabList); %create a vector of matches
+    ind = find(matching_words); 
+    for i = ind
+        word_indices = [word_indices; ind];
+    end
 
 
 
